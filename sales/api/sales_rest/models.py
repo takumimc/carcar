4463cfce58ce9c5b792463bd98e5a848670d9df0
Kevin Nguyen <kevin.nguyen.422@gmail.com>
2023-06-05 16:51:13 -0700

    added salesperson and customer models

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Salesperson(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    employee_id = models.PositiveIntegerField()
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=15)
