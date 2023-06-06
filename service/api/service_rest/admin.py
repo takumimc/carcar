61bd3428216b32798b5f4d0d240eb7c54d065d27
Ian McIntyre <ianmcintyre12@hotmail.com>
2023-06-05 18:20:34 -0700

    poller

--- service/api/service_rest/admin.py
+++ service/api/service_rest/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Technician, Appointment, AutomobileVO
 # Register your models here.
+admin.site.register(Technician)
+admin.site.register(Appointment)
+admin.site.register(AutomobileVO)
--- service/poll/poller.py
+++ service/poll/poller.py
@@ -11,7 +11,7 @@ django.setup()
 
 # Import models from service_rest, here. Ignore vs-code error hinting
 # from service_rest.models import Something
-
+from service_rest.models import AutomobileVO
 
 def poll(repeat=True):
     while True:
@@ -19,15 +19,22 @@ def poll(repeat=True):
         try:
             # Write your polling logic, here
             # Do not copy entire file
-            pass
-        
+            response = requests.get('http://inventory-api:8000/api/automobiles/')
+            automobiles = response.json()
+            # print('request success')
+            # print(automobiles)
+            for automobile in automobiles['autos']:
+                AutomobileVO.objects.update_or_create(
+                    vin=automobile['vin'],
+                    sold=automobile['sold']
+                )
         except Exception as e:
             print(e, file=sys.stderr)
 
         if (not repeat):
             break
 
-        time.sleep(60)
+        time.sleep(5)
 
 
 if __name__ == "__main__":
