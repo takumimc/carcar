71dfbef68ce433ee38777bf57874b05bee4639b0
Kevin Nguyen <kevin.nguyen.422@gmail.com>
2023-06-05 17:01:07 -0700

    added automobileVO and Sale models

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -4,10 +4,29 @@ from django.db import models
 class Salesperson(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    employee_id = models.PositiveIntegerField()
+    employee_id = models.PositiveIntegerField(unique=True)
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=15)
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+    sold = models.BooleanField(default=False)
+
+class Sale(models.Model):
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        on_delete=models.CASCADE
+    )
+    salesperson = models.ForeignKey(
+        Salesperson,
+        on_delete=models.CASCADE,
+    )
+    customer = models.ForeignKey(
+        Customer,
+        on_delete=models.CASCADE,
+    )
+    price = models.PositiveIntegerField()
