ca30c7cd85aacdc79d0ca00fb8bd50cb18e6fee0
Kevin Nguyen <kevin.nguyen.422@gmail.com>
2023-06-06 14:39:47 -0700

    added salespeople list and form to nav

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -34,6 +34,12 @@ function Nav() {
             <li className="nav-item">
               <NavLink className="nav-link" to="/technicians/new">Add Technician</NavLink>
             </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/salespeople">Salespeople</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/salespeople/new">Add a Salesperson</NavLink>
+            </li>
           </ul>
         </div>
       </div>
