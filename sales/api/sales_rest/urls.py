ba3b4203bf9bac3fc33d47c4820db28edd60d593
Kevin Nguyen <kevin.nguyen.422@gmail.com>
2023-06-06 10:33:53 -0700

    added get, post, delete for customers

--- sales/api/sales_rest/urls.py
+++ sales/api/sales_rest/urls.py
@@ -3,10 +3,14 @@ from django.urls import path
 from .views import (
     api_salesperson,
     api_salesperson_delete,
+    api_customers,
+    api_customer_delete,
 )
 
 
 urlpatterns = [
     path("salespeople/", api_salesperson, name="api_salesperson"),
     path("salespeople/<int:pk>", api_salesperson_delete, name="api_salesperson_delete"),
+    path("customers/", api_customers, name="api_customers"),
+    path("customers/<int:pk>", api_customer_delete, name="api_customer_delete"),
 ]
--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -34,3 +34,26 @@ def api_salesperson(request):
 def api_salesperson_delete(request, pk):
     count, _ = Salesperson.objects.filter(id=pk).delete()
     return JsonResponse({"deleted": count > 0})
+
+
+@require_http_methods(["GET", "POST"])
+def api_customers(request):
+    if request.method == "GET":
+        customers = Customer.objects.all()
+        return JsonResponse(
+            {"customers": customers},
+            encoder=CustomerEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+        customer = Customer.objects.create(**content)
+        return JsonResponse(
+            customer,
+            encoder=CustomerEncoder,
+            safe=False,
+        )
+
+@require_http_methods(["DELETE"])
+def api_customer_delete(request, pk):
+    count, _ = Customer.objects.filter(id=pk).delete()
+    return JsonResponse({"deleted": count > 0})
