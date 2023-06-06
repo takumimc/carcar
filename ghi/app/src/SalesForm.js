02b590593b1d2f15c896225ac44040c4b895c11b
Kevin Nguyen <kevin.nguyen.422@gmail.com>
2023-06-06 16:36:19 -0700

    completed sales form frontend

--- ghi/app/src/SalesForm.js
+++ ghi/app/src/SalesForm.js
@@ -4,6 +4,57 @@ function SalesForm(props) {
     const [automobiles, setAutomobiles] = useState([]);
     const [salespeople, setSalespeople] = useState([]);
     const [customers, setCustomers] = useState([]);
+    const [automobile, setAutomobile] = useState('');
+    const [salesperson, setSalesperson] = useState('');
+    const [customer, setCustomer] = useState('');
+    const [price, setPrice] = useState('');
+
+    const handleAutomobileChange = (e) => {
+        const value = e.target.value;
+        setAutomobile(value);
+    }
+
+    const handleSalespersonChange = (e) => {
+        const value = e.target.value;
+        setSalesperson(value);
+    }
+
+    const handleCustomerChange = (e) => {
+        const value = e.target.value;
+        setCustomer(value);
+    }
+
+    const handlePriceChange = (e) => {
+        const value = e.target.value;
+        setPrice(value);
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const data = {};
+        data.automobile = automobile;
+        data.salesperson = salesperson;
+        data.customer = customer;
+        data.price = price;
+
+        const saleUrl = 'http://localhost:8090/api/sales/';
+        const fetchConfig = {
+            method: "post",
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        const response = await fetch(saleUrl, fetchConfig);
+        if (response.ok) {
+            const newSale = await response.json();
+            setAutomobile('');
+            setCustomer('');
+            setSalesperson('');
+            setPrice('');
+        }
+    }
 
     const fetchAutomobiles = async () => {
         const url = 'http://localhost:8100/api/automobiles/'
@@ -47,9 +98,9 @@ function SalesForm(props) {
             <div className="offset-3 col-6">
                 <div className="shadow p-4 mt-4">
                     <h1>Record a New Sale</h1>
-                    <form id="create-customer-form">
+                    <form onSubmit={handleSubmit} id="create-customer-form">
                         <div className="mb-3">
-                            <select required name="automobile" id="automobile" className="form-select">
+                            <select value={automobile} onChange={handleAutomobileChange} required name="automobile" id="automobile" className="form-select">
                                 <option value="">Choose an automobile VIN</option>
                                 {automobiles.filter(autos => autos.sold == false).map(auto => {
                                     return (
@@ -61,7 +112,7 @@ function SalesForm(props) {
                             </select>
                         </div>
                         <div className="mb-3">
-                            <select required name="salesperson" id="salesperson" className="form-select">
+                            <select value={salesperson} onChange={handleSalespersonChange} required name="salesperson" id="salesperson" className="form-select">
                                 <option value="">Choose a salesperson</option>
                                 {salespeople.map(salesperson => {
                                     return (
@@ -73,7 +124,7 @@ function SalesForm(props) {
                             </select>
                         </div>
                         <div className="mb-3">
-                            <select required name="customer" id="customer" className="form-select">
+                            <select value={customer} onChange={handleCustomerChange} required name="customer" id="customer" className="form-select">
                                 <option value="">Choose a customer</option>
                                 {customers.map(customer => {
                                     return (
@@ -85,7 +136,7 @@ function SalesForm(props) {
                             </select>
                         </div>
                         <div className="form-floating mb-3">
-                            <input placeholder="price" required type="number" name="price" id="price" className="form-control"/>
+                            <input value={price} onChange={handlePriceChange} placeholder="price" required type="number" name="price" id="price" className="form-control"/>
                             <label htmlFor="phone_number">Price:</label>
                         </div>
                         <button className="btn btn-primary">Create</button>
