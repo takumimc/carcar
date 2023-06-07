d8d51c3b6b98dfe8a88b06f23645c2cf3ab0a466
Ian McIntyre <ianmcintyre12@hotmail.com>
2023-06-06 17:12:40 -0700

    search history  done

--- ghi/app/src/AppointmentHistory.js
+++ ghi/app/src/AppointmentHistory.js
@@ -3,7 +3,11 @@ import React, {useState, useEffect} from "react";
 function HistoryAppointments(){
     const [err, setErr] = useState('')
     const [appointments, setAppointments] = useState([])
-
+    const [search, setSearch] = useState('')
+    function handleSearch(e) {
+        const value = e.target.value
+        setSearch(value)
+    }
     const loadAppointments = async() =>{
 
         const url = 'http://localhost:8080/api/appointments/'
@@ -31,13 +35,84 @@ function HistoryAppointments(){
         }
     }
 
+    const searchAppointments = (search) => async() =>{
+        const url = 'http://localhost:8080/api/appointments/'
+        const response = await fetch(url)
+        if (!response.ok){
+            const error = await response.json()
+            setErr(error['message'])
+            const errTag = document.getElementById('err-not')
+            errTag.classList.remove('d-none')
+
+        } else {
+            const data = await response.json()
+            // console.log(data)
+            for (let item of data.appointments){
+                if(item.vin !== search){
+                    data.appointments.pop(item)
+                } else {
+                const datetime = new Date(item.date_time)
+                const date = datetime.toLocaleDateString(undefined,{month:'long', day:'numeric',year:'numeric'})
+                const time = datetime.toLocaleTimeString('en-US')
+                // console.log(time)
+                // console.log(datetime)
+                item['time'] = time
+                item['date'] = date
+                }
+            }
+            console.log(data.appointments)
+            setAppointments(data['appointments'])
+            // console.log(data.appointments)
+        }
+    }
+
+    async function submitSearch(e) {
+        e.preventDefault()
+        const url = 'http://localhost:8080/api/appointments/'
+        const response = await fetch(url)
+        if (!response.ok){
+            const error = await response.json()
+            setErr(error['message'])
+            const errTag = document.getElementById('err-not')
+            errTag.classList.remove('d-none')
+
+        } else {
+            const data = await response.json()
+            // console.log(data.appointments)
+            const search_data = []
+            for (let item of data.appointments){
+                if(item.vin === search){
+                search_data.push(item)
+                const datetime = new Date(item.date_time)
+                const date = datetime.toLocaleDateString(undefined,{month:'long', day:'numeric',year:'numeric'})
+                const time = datetime.toLocaleTimeString('en-US')
+                // console.log(time)
+                // console.log(datetime)
+                item['time'] = time
+                item['date'] = date
+                }
+
+            }
+            // console.log(search_data)
+            setAppointments(search_data)
+            // console.log(data.appointments)
+        }
+
+
+    }
 
     useEffect(() => {loadAppointments()},[])
 
     return (
         <div className="container mt-4">
-            <h1>Service Appointments</h1>
+            <h1>Service History</h1>
             <div className='alert alert-warning d-none' id='err-not'>{err}</div>
+            <div>
+                <form onSubmit={submitSearch}>
+                    <input value={search} onChange={handleSearch} name='search' id='search' placeholder='Search by VIN' className='form-control' type='text'></input>
+                    <button>Submit</button>
+                </form>
+            </div>
             <table className="table table-striped">
                 <thead>
                     <tr>
@@ -53,7 +128,6 @@ function HistoryAppointments(){
                 </thead>
                 <tbody>
                     {appointments.map(appointment => {
-                        if (appointment.status !== 'Scheduled')
                         return (
                             <tr key={appointment.id}>
                                 <td>{appointment.vin}</td>
@@ -61,7 +135,7 @@ function HistoryAppointments(){
                                 <td>{appointment.customer}</td>
                                 <td>{appointment.date}</td>
                                 <td>{appointment.time}</td>
-                                <td>{appointment.technician.employee_id}</td>
+                                <td>{appointment.technician.first_name} {appointment.technician.last_name}</td>
                                 <td>{appointment.reason}</td>
                                 <td>{appointment.status}</td>
                             </tr>
