5d70b0927cd5c88eb0b223aef68869392ba5cc37
Ian McIntyre <ianmcintyre12@hotmail.com>
2023-06-08 11:14:33 -0700

    ian m pol update

--- sales/api/sales_rest/admin.py
+++ sales/api/sales_rest/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Sale
+from .models import Sale, AutomobileVO
 
 # Register your models here.
 @admin.register(Sale)
 class SaleAdmin(admin.ModelAdmin):
     pass
+
+
+admin.site.register(AutomobileVO)
--- service/poll/poller.py
+++ service/poll/poller.py
@@ -12,6 +12,7 @@ django.setup()
 # Import models from service_rest, here. Ignore vs-code error hinting
 # from service_rest.models import Something
 from service_rest.models import AutomobileVO
+from django.db import IntegrityError
 
 def poll(repeat=True):
     while True:
@@ -24,17 +25,23 @@ def poll(repeat=True):
             # print('request success')
             # print(automobiles)
             for automobile in automobiles['autos']:
-                AutomobileVO.objects.update_or_create(
-                    vin=automobile['vin'],
-                    sold=automobile['sold']
-                )
+                try:
+                    AutomobileVO.objects.update_or_create(
+                        vin=automobile['vin'],
+                        sold=automobile['sold'],
+                    )
+                except IntegrityError as e:
+                    if 'unique constraint' in e.args[0]:
+                        auto = AutomobileVO.objects.get(vin=automobile['vin'])
+                        auto.sold=automobile['sold']
+                        auto.save()
         except Exception as e:
             print(e, file=sys.stderr)
 
         if (not repeat):
             break
 
-        time.sleep(20)
+        time.sleep(5)
 
 
 if __name__ == "__main__":
